960cb977dd9e14e536d4afb419524d592eac05b4
Neil Burningham <neil.burningham@mediakind.com>
2022-06-10 09:58:22 +0100

    Adding real functionality (solo...)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
 use std::collections::HashMap;
 
-fn main() {
+#[derive(Debug)]
+enum AppErrors {
+    SaveError(std::io::Error),
+}
+
+fn main() -> Result<(), AppErrors> {
     let mut args = std::env::args().skip(1);
     let key = args.next().unwrap();
     let val = args.next().unwrap();
 
-    let contents = format!("{}\t{}\n", key, val);
-
-    std::fs::write("kv.db", contents).expect("Failed to write database");
-
     println!("{} <- {}", key, val);
 
-    let database: Database = Database::new().unwrap();
+    let mut database: Database = Database::new();
 
     println!("{:?}", database);
+
+    database.add_key(key, val);
+
+    println!("{:?}", database);
+
+    database.save()?;
+
+    Ok(())
 }
 
 #[derive(Debug)]
@@ -22,15 +31,37 @@ struct Database {
 }
 
 impl Database {
-    fn new() -> Result<Self, std::io::Error> {
+    fn new() -> Self {
         let mut map: HashMap<String, String> = HashMap::new();
-        let contents = std::fs::read_to_string("kv.db")?;
-        for line in contents.lines() {
-            let (key, val) = line.split_once('\t').expect("error!");
-            map.insert(key.to_owned(), val.to_owned());
+        match std::fs::read_to_string("kv.db") {
+            Ok(contents) => {
+                for line in contents.lines() {
+                    let (key, val) = line.split_once('\t').expect("error!");
+                    map.insert(key.to_owned(), val.to_owned());
+                }
+                Database {
+                    map: map
+                }
+            }
+            Err(_e) => {
+                Database {
+                    map: HashMap::new()
+                }
+            }
         }
-        Ok(Database {
-            map: map
-        })
+    }
+
+    fn save(&self) -> Result<(), AppErrors> {
+        let mut contents: String = String::new();
+        for (key, val) in self.map.iter() {
+            contents.push_str(&format!("{}\t{}\n", key, val));
+        }
+        
+        std::fs::write("kv.db", contents).expect("Failed to write database");
+        Ok(())
+    }
+
+    fn add_key(&mut self, key: String, val: String) {
+        self.map.insert(key, val);
     }
 }
